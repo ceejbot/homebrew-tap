91bb156e6622006fccedf7b8419db24c79048d1d
C J Silverio <ceejceej@gmail.com>
2022-08-17 23:51:23 -0700

    Release v0.3.0.

--- tomato.rb
+++ tomato.rb
@@ -4,13 +4,13 @@
 class Tomato < Formula
   desc "🍅 A command-line tool to get and set values in toml files while preserving comments and formatting."
   homepage "https://github.com/ceejbot/tomato"
-  version "0.2.0"
+  version "0.3.0"
   license "BlueOak-1.0.0"
 
   on_macos do
 	if Hardware::CPU.intel?
-	  url "https://github.com/ceejbot/tomato/releases/download/v0.2.0/tomato_intel_darwin.tar.gz"
-	  sha256 "3339e67aadbff60298b5ebc5a378c9b5543e6b0b9ea1db9f1a59e41ce7f8eb0e"
+	  url "https://github.com/ceejbot/tomato/releases/download/v0.3.0/tomato_intel_darwin.tar.gz"
+	  sha256 "343c45d5d1432cb8ffeebba25d8fc48ae403ee75b6dd7593cd7d989302b5a10c"
 
 	  def install
 	  	bin.install "tomato" => "tomato"
@@ -20,8 +20,8 @@ class Tomato < Formula
 	  end
 	end
 	if Hardware::CPU.arm?
-	  url "https://github.com/ceejbot/tomato/releases/download/v0.2.0/tomato_aarch64_darwin.tar.gz"
-	  sha256 "f0fc70a21e4b85663535896365598c7b248f161221542e3475a3a9ae0c6bbbfd"
+	  url "https://github.com/ceejbot/tomato/releases/download/v0.3.0/tomato_aarch64_darwin.tar.gz"
+	  sha256 "206f267186b00e1569b3a66604d06818ab463df30fe70343560873e592f36163"
 
 	  def install
 		bin.install "tomato" => "tomato"
@@ -34,8 +34,8 @@ class Tomato < Formula
 
   on_linux do
 	if Hardware::CPU.intel?
-	  url "https://github.com/ceejbot/tomato/releases/download/v0.2.0/tomato_amd64_linux.tar.gz"
-	  sha256 "db6ffefdaef6b97d2fce2b9f9f1d56a42c7c45aa5ba86458b72ff08179199ac4"
+	  url "https://github.com/ceejbot/tomato/releases/download/v0.3.0/tomato_amd64_linux.tar.gz"
+	  sha256 "715a74b7ac71bda4b897bd67c55a475f47e0d94674d9c487e0950e3dcc603b54"
 
 	  def install
 		bin.install "tomato" => "tomato"
@@ -45,8 +45,8 @@ class Tomato < Formula
 	  end
 	end
 	if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-	  url "https://github.com/ceejbot/tomato/releases/download/v0.2.0/tomato_arm64_linux.tar.gz"
-	  sha256 "354be1b2132526f8bcd061a3e8934d220bb0cc1a69567406d4bac00ca84d0829"
+	  url "https://github.com/ceejbot/tomato/releases/download/v0.3.0/tomato_arm64_linux.tar.gz"
+	  sha256 "a40e0c7572340ce2788bb8e4e47c518f893404dc66ce0d38ea704599c52d5a38"
 
 	  def install
 		bin.install "tomato" => "tomato"
