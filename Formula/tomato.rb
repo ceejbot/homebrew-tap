717697c99c450d2460ef668bfa5800e401dcded1
C J Silverio <ceejceej@gmail.com>
2025-06-15 13:17:47 -0700

    Patch.

--- Formula/tomato.rb
+++ Formula/tomato.rb
@@ -3,74 +3,21 @@ require "utils/formatter"
 require "utils/github"
 require "system_command"
 
-class GitHubCliDownloadStrategy < CurlDownloadStrategy
-	require "utils/formatter"
-	require "utils/github"
-	require "system_command"
-
-	def initialize(url, name, version, **meta)
-	    super
-	    # Extract owner and repo from the URL
-	    # Example: https://github.com/ceejbot/formulaic/releases/download/main/formulaic-aarch64-apple-darwin.tar.gz
-	    match_data = %r{^https?://github\.com/(?<owner>[^/]+)/(?<repo>[^/]+)/releases/download}.match(@url)
-	    return unless match_data
-
-	    @owner = match_data[:owner]
-	    @repo = match_data[:repo]
-	    @filename = File.basename(@url)
-	end
-
-	def fetch(timeout: nil)
-		ohai "Downloading #{url} using GitHub CLI"
-		if cached_location.exist?
-		    puts "Already downloaded: #{cached_location}"
-		else
-			begin
-			  	# Create the temporary directory
-			  	temporary_path.dirname.mkpath
-
-			  	# Use gh CLI to download the release asset
-			  	system_command("/opt/homebrew/bin/gh", args: [
-				   		"release", "download",
-						"-R", "#{@owner}/#{@repo}",
-				   		"--pattern", "#{@filename}",
-						"-D", "#{temporary_path}"
-				     ], print_stderr: true)
-			rescue ErrorDuringExecution
-        		raise GitHubCliDownloadStrategyError, "GitHub CLI download failed for: #{url}"
-      		end
-			cached_location.dirname.mkpath
-
-			# Find the downloaded file in the temporary path
-			downloaded_file = Dir["#{temporary_path}/*"].first
-
-			if downloaded_file
-				FileUtils.mv(downloaded_file, cached_location)
-			else
-				raise GitHubCliDownloadStrategyError, "Downloaded file not found in #{temporary_path}"
-			end
-		end
-
-		symlink_location.dirname.mkpath
-    	FileUtils.ln_s cached_location.relative_path_from(symlink_location.dirname), symlink_location, force: true
-	end
-end
-
-class Tomato Toml < Formula
+class Tomato < Formula
     desc "🍅 A command-line tool to get and set values in toml files while preserving comments and formatting."
     homepage "https://github.com/ceejbot/tomato"
     version "0.5.1"
     license "BlueOak-1.0.0"
     if OS.mac? && Hardware::CPU.arm?
-        url    "https://github.com/ceejbot/tomato/releases/download/v0.5.1/tomato-aarch64-apple-darwin.tar.gz", using: GitHubCliDownloadStrategy
+        url    "https://github.com/ceejbot/tomato/releases/download/v0.5.1/tomato-aarch64-apple-darwin.tar.gz"
         sha256 "10837e772d4e836035d4e7bfe2462136c3669534090aeb8c8f2cf65a0eb75c2d"
     end
     if OS.mac? && Hardware::CPU.intel?
-        url    "https://github.com/ceejbot/tomato/releases/download/v0.5.1/tomato-x86_64-apple-darwin.tar.gz", using: GitHubCliDownloadStrategy
+        url    "https://github.com/ceejbot/tomato/releases/download/v0.5.1/tomato-x86_64-apple-darwin.tar.gz"
         sha256 "312e6198b129b5e412d1817b6f88bf3595821566817023ac2a1eac5db12f2aaa"
     end
     if OS.linux? && Hardware::CPU.intel?
-        url    "https://github.com/ceejbot/tomato/releases/download/v0.5.1/tomato-x86_64-unknown-linux-gnu.tar.gz", using: GitHubCliDownloadStrategy
+        url    "https://github.com/ceejbot/tomato/releases/download/v0.5.1/tomato-x86_64-unknown-linux-gnu.tar.gz"
         sha256 "f9c5c4841922185d3c9c560c5f1fa8d729c9bb5190dae96fc0e870a7bef4fbeb"
     end
 
