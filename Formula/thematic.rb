ff0de2379a42f65bc94dd686dda12a7c9465d691
formula robot <ceejceej@gmail.com>
2025-07-10 05:52:16 +0000

    thematic v0.1.2

--- Formula/thematic.rb
+++ Formula/thematic.rb
@@ -1,15 +1,15 @@
 class Thematic < Formula
     desc "Convert themes between VSCode and Zed formats"
     homepage "https://github.com/ceejbot/thematic"
-    version "0.1.1"
+    version "0.1.2"
     license "Parity-7.0.0"
     if OS.mac? && Hardware::CPU.arm?
-        url    "https://github.com/ceejbot/thematic/releases/download/v0.1.1/thematic-aarch64-apple-darwin.tar.gz"
-        sha256 "6cc625a27e68a3e04c408ec356e0a3180d6c81f0644fa21d6a5971a901baf7f5"
+        url    "https://github.com/ceejbot/thematic/releases/download/v0.1.2/thematic-aarch64-apple-darwin.tar.gz"
+        sha256 "8f50eb8db65d33e409f1f03ade7187d3a4612e6d77ef0d75e9152a89a0ca0a0e"
     end
     if OS.mac? && Hardware::CPU.intel?
-        url    "https://github.com/ceejbot/thematic/releases/download/v0.1.1/thematic-x86_64-apple-darwin.tar.gz"
-        sha256 "cf0199db8d4fc045e11a4b829649f0afab865b1e0084e2941defeb1cf16e87b6"
+        url    "https://github.com/ceejbot/thematic/releases/download/v0.1.2/thematic-x86_64-apple-darwin.tar.gz"
+        sha256 "814c50b06e185d45efd349d3d64a29ebf263c5365aa0657d6c2bf74140adbde7"
     end
 
     BINARY_ALIASES = {
