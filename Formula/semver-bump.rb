07cc6dc82335e0866e6781dfb2d1ac7261502792
formula robot <ceejceej@gmail.com>
2025-08-22 07:06:01 +0000

    semver-bump v2.0.0

--- Formula/semver-bump.rb
+++ Formula/semver-bump.rb
@@ -1,58 +1,50 @@
 class SemverBump < Formula
-  desc "A command-line tool for bumping semver-compliant version numbers"
-  homepage "https://github.com/ceejbot/semver-bump"
-  version "1.0.1"
-  if OS.mac?
-    if Hardware::CPU.arm?
-      url "https://github.com/ceejbot/semver-bump/releases/download/1.0.1/semver-bump-aarch64-apple-darwin.tar.xz"
-      sha256 "3415b3fadd135565d120ff5b0a1ef7967afaece63f959f4e059903b386cafd06"
+    desc "A command-line tool for bumping semver-compliant version numbers"
+    homepage "https://github.com/ceejbot/semver-bump"
+    version "2.0.0"
+    license "Parity-7.0.0"
+    if OS.mac? && Hardware::CPU.arm?
+        url    "https://github.com/ceejbot/semver-bump/releases/download/v2.0.0/semver-bump-aarch64-apple-darwin.tar.gz"
+        sha256 "1cf3c76a3bfe01afe5fc6b362905dd576ad283f104bc55a455722cfad2e55686"
     end
-    if Hardware::CPU.intel?
-      url "https://github.com/ceejbot/semver-bump/releases/download/1.0.1/semver-bump-x86_64-apple-darwin.tar.xz"
-      sha256 "70605249ae118813250e5e95f5f452b97da070c9a7c305c5a96d3a33a64ccfb6"
+    if OS.mac? && Hardware::CPU.intel?
+        url    "https://github.com/ceejbot/semver-bump/releases/download/v2.0.0/semver-bump-x86_64-apple-darwin.tar.gz"
+        sha256 "6689185edcf256e3d11281a22aff74f0db1c750c4f17ca33f2a44560b5bab356"
     end
-  end
-  if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/ceejbot/semver-bump/releases/download/1.0.1/semver-bump-x86_64-unknown-linux-gnu.tar.xz"
-    sha256 "644af381411d23dd253e6f66e7dd4adbad669eb768b0bc821b244c8d479c3063"
-  end
-  license "Parity-7.0.0"
-
-  BINARY_ALIASES = {
-    "aarch64-apple-darwin":     {},
-    "x86_64-apple-darwin":      {},
-    "x86_64-pc-windows-gnu":    {},
-    "x86_64-unknown-linux-gnu": {},
-  }.freeze
-
-  def target_triple
-    cpu = Hardware::CPU.arm? ? "aarch64" : "x86_64"
-    os = OS.mac? ? "apple-darwin" : "unknown-linux-gnu"
-
-    "#{cpu}-#{os}"
-  end
-
-  def install_binary_aliases!
-    BINARY_ALIASES[target_triple.to_sym].each do |source, dests|
-      dests.each do |dest|
-        bin.install_symlink bin/source.to_s => dest
-      end
+    if OS.linux? && Hardware::CPU.intel?
+        url    "https://github.com/ceejbot/semver-bump/releases/download/v2.0.0/semver-bump-x86_64-unknown-linux-gnu.tar.gz"
+        sha256 "8a1e5b9626685455acc9b0d8b7f2e787135b6afea97825ed6dade5b83b764fb9"
     end
-  end
 
-  def install
-    bin.install "semver-bump" if OS.mac? && Hardware::CPU.arm?
-    bin.install "semver-bump" if OS.mac? && Hardware::CPU.intel?
-    bin.install "semver-bump" if OS.linux? && Hardware::CPU.intel?
+    BINARY_ALIASES = {
+        "aarch64-apple-darwin":     {},
+        "x86_64-apple-darwin":      {},
+        "aarch64-unknown-linux-gnu": {},
+        "x86_64-unknown-linux-gnu": {},
+    }.freeze
 
-    install_binary_aliases!
+    def target_triple
+        cpu = Hardware::CPU.arm? ? "aarch64" : "x86_64"
+        os = OS.mac? ? "apple-darwin" : "unknown-linux-gnu"
+        "#{cpu}-#{os}"
+    end
 
-    # Homebrew will automatically install these, so we don't need to do that
-    doc_files = Dir["README.*", "readme.*", "LICENSE", "LICENSE.*", "CHANGELOG.*"]
-    leftover_contents = Dir["*"] - doc_files
+    def install_binary_aliases!
+        BINARY_ALIASES[target_triple.to_sym].each do |source, dests|
+            dests.each do |dest|
+            bin.install_symlink bin/source.to_s => dest
+            end
+        end
+    end
 
-    # Install any leftover files in pkgshare; these are probably config or
-    # sample files.
-    pkgshare.install(*leftover_contents) unless leftover_contents.empty?
-  end
+    def install
+        bin.install "semver-bump" if OS.mac? && Hardware::CPU.arm?
+        bin.install "semver-bump" if OS.mac? && Hardware::CPU.intel?
+        bin.install "semver-bump" if OS.linux? && Hardware::CPU.intel?
+
+        install_binary_aliases!
+        doc_files = Dir["README.*", "readme.*", "LICENSE", "LICENSE.*", "CHANGELOG.*"]
+        leftover_contents = Dir["*"] - doc_files
+        pkgshare.install(*leftover_contents) unless leftover_contents.empty?
+    end
 end
