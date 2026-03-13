8d8811b2eb0f50b10add2c23b026abce3ff260a7
formula robot <ceejceej@gmail.com>
2026-03-13 04:15:42 +0000

    formulaic v0.7.0

--- Formula/formulaic.rb
+++ Formula/formulaic.rb
@@ -1,19 +1,19 @@
 class Formulaic < Formula
-    desc "Update a Homebrew tap formula for a Rust project from its latest github release."
+    desc "Update a Homebrew tap formula for a project from a manifest or Cargo.toml."
     homepage "https://github.com/ceejbot/formulaic"
-    version "0.6.0"
+    version "0.7.0"
     license "Parity-7.0.0"
     if OS.mac? && Hardware::CPU.arm?
-        url    "https://github.com/ceejbot/formulaic/releases/download/v0.6.0/formulaic-aarch64-apple-darwin.tar.gz"
-        sha256 "93ec4812ae16d6787938a452033dd870bc1260df4af4c6324a559c018efb2fa8"
+        url    "https://github.com/ceejbot/formulaic/releases/download/v0.7.0/formulaic-aarch64-apple-darwin.tar.gz"
+        sha256 "11d0d9c461e1e52c69c4774285e96af6229bcf1a1a4aad1521f1cd78eb2afdda"
     end
     if OS.mac? && Hardware::CPU.intel?
-        url    "https://github.com/ceejbot/formulaic/releases/download/v0.6.0/formulaic-x86_64-apple-darwin.tar.gz"
-        sha256 "bb97f4a098ccb2c1dbc2436668b81703ca7d2f001a9692a913ffb0cd98b409e9"
+        url    "https://github.com/ceejbot/formulaic/releases/download/v0.7.0/formulaic-x86_64-apple-darwin.tar.gz"
+        sha256 "c5f509d78e393a7b75b01309015d1edc8860595bfd235e755594381673d1436b"
     end
     if OS.linux? && Hardware::CPU.intel?
-        url    "https://github.com/ceejbot/formulaic/releases/download/v0.6.0/formulaic-x86_64-unknown-linux-gnu.tar.gz"
-        sha256 "153e01086ddc756ea08aa9007bc05a0d37cab8de7f1df3bba383d96c9895a24f"
+        url    "https://github.com/ceejbot/formulaic/releases/download/v0.7.0/formulaic-x86_64-unknown-linux-gnu.tar.gz"
+        sha256 "9f3e0475f3b85ea699f5415b1062ef4a414666beb2bd3f70a4177c94acc68f0e"
     end
 
     BINARY_ALIASES = {
@@ -38,9 +38,15 @@ class Formulaic < Formula
     end
 
     def install
-        bin.install "formulaic" if OS.mac? && Hardware::CPU.arm?
-        bin.install "formulaic" if OS.mac? && Hardware::CPU.intel?
-        bin.install "formulaic" if OS.linux? && Hardware::CPU.intel?
+        if OS.mac? && Hardware::CPU.arm?
+            bin.install "formulaic"
+        end
+        if OS.mac? && Hardware::CPU.intel?
+            bin.install "formulaic"
+        end
+        if OS.linux? && Hardware::CPU.intel?
+            bin.install "formulaic"
+        end
 
         install_binary_aliases!
         doc_files = Dir["README.*", "readme.*", "LICENSE", "LICENSE.*", "CHANGELOG.*"]
