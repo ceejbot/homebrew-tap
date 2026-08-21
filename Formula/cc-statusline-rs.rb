533459312b67e545f40ec10f68fa3e1e08427384
formula robot <ceejceej@gmail.com>
2026-08-21 06:34:58 +0000

    cc-statusline-rs v0.7.0

--- Formula/cc-statusline-rs.rb
+++ Formula/cc-statusline-rs.rb
@@ -1,23 +1,23 @@
 class CcStatuslineRs < Formula
     desc "An opinionated ANSI statusline for Claude Code"
     homepage ""
-    version "0.6.1"
+    version "0.7.0"
     license "Apache-2.0"
     if OS.mac? && Hardware::CPU.arm?
-        url    "https://github.com/ceejbot/cc-statusline-rs/releases/download/v0.6.1/cc-statusline-rs-aarch64-apple-darwin.tar.gz"
-        sha256 "3eca05eeaf56235fec1e9ca18a2bbd8d01b30c30aeccb9642f647b293aaf09c4"
+        url    "https://github.com/ceejbot/cc-statusline-rs/releases/download/v0.7.0/cc-statusline-rs-aarch64-apple-darwin.tar.gz"
+        sha256 "fab0ca6bc5cbd9b31a6bf1d9c2444e8a43607f79688d56a1eefa88e75ae5f9ac"
     end
     if OS.linux? && Hardware::CPU.arm?
-        url    "https://github.com/ceejbot/cc-statusline-rs/releases/download/v0.6.1/cc-statusline-rs-aarch64-unknown-linux-gnu.tar.gz"
-        sha256 "41b49f594754d13af77727674558c3c669d6dc2149b657a7e4be04273017c58f"
+        url    "https://github.com/ceejbot/cc-statusline-rs/releases/download/v0.7.0/cc-statusline-rs-aarch64-unknown-linux-gnu.tar.gz"
+        sha256 "51e5213472e25eb9db3356103f064bf5142f7abd0cb0d727d5f318151cdf5756"
     end
     if OS.mac? && Hardware::CPU.intel?
-        url    "https://github.com/ceejbot/cc-statusline-rs/releases/download/v0.6.1/cc-statusline-rs-x86_64-apple-darwin.tar.gz"
-        sha256 "50845ed2838830e9e16d19c3c38c831f54ee5c788ea4f21a069d89e5026e1fed"
+        url    "https://github.com/ceejbot/cc-statusline-rs/releases/download/v0.7.0/cc-statusline-rs-x86_64-apple-darwin.tar.gz"
+        sha256 "6cfd2ac63c2e8010e4e45f4491db03777ca8a4add8b58151adb8a7436e939ec7"
     end
     if OS.linux? && Hardware::CPU.intel?
-        url    "https://github.com/ceejbot/cc-statusline-rs/releases/download/v0.6.1/cc-statusline-rs-x86_64-unknown-linux-gnu.tar.gz"
-        sha256 "d2dc94f5f58b04c58c839d5d9056fb68c7192d8a357db70d9152e046817f9ab9"
+        url    "https://github.com/ceejbot/cc-statusline-rs/releases/download/v0.7.0/cc-statusline-rs-x86_64-unknown-linux-gnu.tar.gz"
+        sha256 "f00d07e463e0471f53a79727e7530e4e7f05e938edcdce72c15a252bcafe3501"
     end
 
     def install
